df5ea7c9413bc99c5f478dde54ac6d32390af701
Nikolife13 <nikolife13@gmail.com>
2026-06-30 10:30:00 +0100

    Set up Spring Boot project scaffolding

---
+++ backend-java/src/main/java/com/smartpharma/SmartpharmaApplication.java
@@ -0,0 +1,13 @@
+package com.smartpharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartpharmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartpharmaApplication.class, args);
+	}
+
+}
